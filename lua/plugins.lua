471294d3bec1d6eae54c25832542a1aaa68a911d
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2026-02-08 12:42:14 +0700

    return packages

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,1 +1,11 @@
-return {}
+return {
+  require("plugins.catppuccin"),
+  require("plugins.devicons"),
+  require("plugins.fzf"),
+  require("plugins.lazygit"),
+  require("plugins.lsp-config"),
+  require("plugins.lualine"),
+  require("plugins.neotree"),
+  require("plugins.toggleterm"),
+  require("plugins.treesitter"),
+}
